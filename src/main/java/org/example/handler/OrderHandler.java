25ab988062435df0885a90abdb68843dd3ab6f71
chern <chernihov.g@gmail.com>
2025-05-26 15:14:18 +0500

    push notifications added

--- src/main/java/org/example/handler/OrderHandler.java
+++ src/main/java/org/example/handler/OrderHandler.java
@@ -2,6 +2,8 @@ package org.example.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.model.UserOrderDto;
+import org.example.pushNotification.NotifyController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +11,12 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class OrderHandler {
 
+    @Autowired
+    private NotifyController notifyController;
+
     @KafkaListener(topics = "${kafka.topic.order-created}", groupId = "order-service-group")
     void getOrder(UserOrderDto userOrderDto) {
         log.info("message received");
-
+        notifyController.sendNotification(userOrderDto);
     }
 }
