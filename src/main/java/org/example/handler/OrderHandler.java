073ca598cd339e88a8fc3d1002d4082407f26ef6
chern <chernihov.g@gmail.com>
2025-05-26 14:57:34 +0500

    message kafka consumer added

--- src/main/java/org/example/handler/OrderHandler.java
+++ src/main/java/org/example/handler/OrderHandler.java
@@ -1,4 +1,17 @@
 package org.example.handler;
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.model.UserOrderDto;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class OrderHandler {
+
+    @KafkaListener(topics = "${kafka.topic.order-created}", groupId = "order-service-group")
+    void getOrder(UserOrderDto userOrderDto) {
+        log.info("message received");
+
+    }
 }
